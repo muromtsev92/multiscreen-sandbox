f7dd6faf3051e4ba677fe715ca2e0bdd4055900e
Sergei Muromtsev <k29393@yandex.ru>
2024-03-12 16:03:10 +0500

    third screen

--- app/src/main/java/com/example/multiscreen_sandbox/MessageFragment.kt
+++ app/src/main/java/com/example/multiscreen_sandbox/MessageFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +14,14 @@ class MessageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+
+        val encryptButton = view.findViewById<Button>(R.id.next)
+
+        encryptButton.setOnClickListener{
+            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
+        }
+
+        return view
     }
 }
