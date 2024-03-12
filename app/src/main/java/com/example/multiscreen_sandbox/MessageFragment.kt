dd3c4db5c95596d26763c1535b70c1d3da5c1a2c
Sergei Muromtsev <k29393@yandex.ru>
2024-03-12 17:44:08 +0500

    safe args attempt

--- app/src/main/java/com/example/multiscreen_sandbox/MessageFragment.kt
+++ app/src/main/java/com/example/multiscreen_sandbox/MessageFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
@@ -18,10 +19,14 @@ class MessageFragment : Fragment() {
 
         val encryptButton = view.findViewById<Button>(R.id.next)
 
-        encryptButton.setOnClickListener{
-            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
-        }
+        val messageView = view.findViewById<EditText>(R.id.message)
 
+        encryptButton.setOnClickListener{
+            val message = messageView.text.toString()
+            val action = MessageFragmentDirections
+                .actionMessageFragmentToEncryptFragment(message)
+            view.findNavController().navigate(action)
+        }
         return view
     }
 }
